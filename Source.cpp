bdb13c5fd07cecbb2d826328ba757eace62a52a7
npax1 <prakh55555@gmail.com>
2023-02-28 19:49:59 +0200

    add

--- Source.cpp
+++ Source.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 
 using namespace std;
 
 int main() {
-	char str[] = "Tenitiate massnge of mode 3, adel Y, whiiffein ser, anctionality.";
-	int counter_number = 0;
-	int counter_letter = 0;
-	int counter_other;
-	for (size_t i = 0; str[i] != '\0'; i++)
+	string str;
+	cout << "Enter a text -> ";
+	getline(cin, str);
+	string rev_str = str;
+	reverse(rev_str.begin(), rev_str.end());
+	if (rev_str == str)
 	{
-		if (isdigit(str[i]))
-		{
-			counter_number++;
-		}
-		if (isalpha(str[i]))
-		{
-			counter_letter++;
-		}
+		cout << "\n" << str << " is palindrom.";
 	}
-	counter_other = strlen(str) - counter_letter - counter_number;
-	cout << "\nNumbers [" << counter_number << "]";
-	cout << "\nLetters [" << counter_letter << "]";
-	cout << "\nOther symbols [" << counter_other << "]";
+	else
+	{
+		cout << "\n" << str << " is not palindrom.";
+	}
 }
